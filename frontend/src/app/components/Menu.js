cb70e2a6635c31f2e1546f6388e50743717c439e
davewrld <keithdave23@gmail.com>
2025-01-25 22:29:24 +0300

    menu bar component

---
+++ frontend/src/app/components/Menu.js
@@ -0,0 +1,34 @@
+"use client";
+import Image from "next/image";
+import { useState } from "react";
+import Link from "next/link";
+
+const Menu = () => {
+  /** Create state **/
+  const [open, setOpen] = useState(false);
+
+  return (
+    <div className="relative">
+      <Image
+        src="/menu.png"
+        alt="Menu"
+        width={28}
+        height={28}
+        className="cursor-pointer"
+        onClick={() => setOpen((prev) => !prev)}
+      />
+      {open && (
+        <div className="absolute bg-black text-white left-0 top-20 w-full h-[calc(100vh-80px)] flex flex-col items-center justify-center gap-8 text-xl z-10">
+          <Link href="/">Home</Link>
+          <Link href="/">Shop</Link>
+          <Link href="/">About</Link>
+          <Link href="/">Contact</Link>
+          <Link href="/">Logout</Link>
+          <Link href="/">Cart (0)</Link>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default Menu
